7b9faf82a27b627f55a0a45ec0232d0faba8947a
Michael Albonetti <michaelalbonetti@Michaels-MacBook-Pro.local>
2017-07-19 07:21:09 -0500

    cleanup get request, add $.when to listen for get completion, and concat results into single array of objects

--- content/themes/casper-mod.0.0.1/assets/js/search.js
+++ content/themes/casper-mod.0.0.1/assets/js/search.js
@@ -7,28 +7,55 @@
 
 jQuery(document).ready(function (){
 
-    console.log("search js file");
+    // -- post and tag data pulled from the server --
+    var postAndTagData = [];
 
+    // -- AJAX get request for the blog tags, using the ghost.url.api to generate the link --
     function getTags() {
         $.get(
             ghost.url.api('tags', {limit: 'all', fields: 'uuid, name'})
         ).done(function (data) {
-            console.log(data);
+            postAndTagData = postAndTagData.concat(data.tags);
         }).error(function (err) {
             console.log(err);
         })
     }
 
+    // -- AJAX get request for the blog post, using the ghost.url.api to generate the link --
     function getPost() {
         $.get(
              ghost.url.api('posts', {limit: 'all', fields: 'uuid, author, title, created_at, url, featured, markdown, tags', include: "author, tags"})
         ).done(function (data) {
-            console.log(data);
+           postAndTagData = postAndTagData.concat(data.posts);
         }).fail(function (err) {
             console.log(err);
         })
     }
 
-    getTags();
-    getPost();
+    // jQuery promise (when) to notify that the data exist and the rest of the functionality can continue, wrapped in a function that is either called onLoad or when the user initiates the search
+    function initSearch() {
+        $.when(getPost(), getTags()).done(function () {
+            setTimeout(function () {
+                console.log(postAndTagData);
+            }, 1);
+        })
+    }
+
+
+
+    // initialize the search div
+
+    // append returned results to the div
+
+
+    // filter the list by name, author, and tags
+    
+    function filter(value) {
+        return postAndTagData.filter(function (item) {
+            return item.name == value || item.tags.name == value || item.author.name == value;
+        })
+    }
+
+
+
 });
